3a25ed4e94f577da60d47bfc3a601fb93060f173
lnicolle <lnicolle@wanadev.fr>
2018-11-20 17:57:30 +0100

    add index.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+const CallbackManager = require("./lib/callback-manager");
+const ConfigStorage = require("./lib/config-storage");
+const MainLoop = require("./lib/mainloop");
+const Debug = require("./lib/debug");
+
+module.exports = {
+  CallbackManager,
+  ConfigStorage,
+  MainLoop,
+  Debug
+};
