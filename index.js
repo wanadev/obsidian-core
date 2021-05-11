22997623a46e4eec7cfa1d9d928ffdcc8666cfce
Victor Serain <vserain@wanadev.fr>
2021-05-11 10:49:21 +0200

    expose classUtils

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const CallbackManager = require("./lib/callback-manager");
 const ConfigStorage = require("./lib/config-storage");
 const MainLoop = require("./lib/mainloop");
 const Debug = require("./lib/debug");
+const classUtils = require("./helpers/class-utils");
 
 module.exports = {
     CallbackManager,
     ConfigStorage,
     MainLoop,
-    Debug
+    Debug,
+    classUtils,
 };
